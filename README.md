afaf1757d1e173ae56d4437b72fb2e5af32e6010
sungmuk Kang <miranaky@gmail.com>
2021-08-29 19:35:33 +0900

    #3-1. Heart size animation(grow and shrink).

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ I use Flutter version 2.2.3 and Dart version 2.13.4.
 I start this project to learn the grammar that has changed since Flutter version 2.2.
 
 1. Title animation : with using TweenAnimationBuilder
-2. Hero Animaion :
+2. Hero Animaion : go to menu detail page with hero animation.
+3. toggle Fav button with color change animation.
+   1. Heart size Grow and Shrink.
+4.
--- lib/Widgets/heart.dart
+++ lib/Widgets/heart.dart
@@ -5,16 +5,63 @@ class Heart extends StatefulWidget {
   _HeartState createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
+  bool isFav = false;
+  late AnimationController _controller;
+  late Animation<Color?> _colorToggleAnimation;
+  late Animation<double?> _sizeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    _colorToggleAnimation =
+        ColorTween(begin: Colors.grey[400], end: Colors.pink)
+            .animate(_controller);
+    _sizeAnimation = TweenSequence<double>(<TweenSequenceItem<double>>[
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 30, end: 50), weight: 50),
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 50, end: 30), weight: 50),
+    ]).animate(_controller);
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          isFav = true;
+        });
+      }
+      if (status == AnimationStatus.dismissed) {
+        setState(() {
+          isFav = false;
+        });
+      }
+      print(status);
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(
-        Icons.favorite,
-        color: Colors.grey[400],
-        size: 30,
-      ),
-      onPressed: () {},
+    return AnimatedBuilder(
+      animation: _colorToggleAnimation,
+      builder: (BuildContext context, _) {
+        return IconButton(
+          icon: Icon(
+            Icons.favorite,
+            color: _colorToggleAnimation.value,
+            size: _sizeAnimation.value,
+          ),
+          onPressed: () {
+            isFav ? _controller.reverse() : _controller.forward();
+          },
+        );
+      },
     );
   }
 }
